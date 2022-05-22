431bd681d84735807bccf540fb7e3ebf2b583379
mikaalat <mikaalat@hotmail.com>
2022-05-22 14:06:38 +0300

    Added scss, extra, js

---
+++ script.js
@@ -0,0 +1,14 @@
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclick = function(){
+      if(content.className == "open"){
+          //shrink the box
+          content.className = "";
+          button.innerHTML ="Näytä lisää";
+      }else{
+          //expand the box
+          content.className = "open";
+          button.innerHTML = "Näytä vähemmän";
+      }
+}
